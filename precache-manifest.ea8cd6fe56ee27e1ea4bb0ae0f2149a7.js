721417b902cb6d5d1a823f33f370ec1bdbb79508
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-02-11 02:38:02 +0300

    Updates

---
+++ precache-manifest.ea8cd6fe56ee27e1ea4bb0ae0f2149a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e544aa680eb60b852726",
+    "url": "/react-todos/static/css/main.1a51caf9.chunk.css"
+  },
+  {
+    "revision": "e544aa680eb60b852726",
+    "url": "/react-todos/static/js/main.e544aa68.chunk.js"
+  },
+  {
+    "revision": "3d16f01b9210c227443d",
+    "url": "/react-todos/static/js/1.3d16f01b.chunk.js"
+  },
+  {
+    "revision": "23b2d069d95a08146c09",
+    "url": "/react-todos/static/js/runtime~main.23b2d069.js"
+  },
+  {
+    "revision": "e6b194c4f0b5132d77bed585fb4a8bd9",
+    "url": "/react-todos/index.html"
+  }
+];
